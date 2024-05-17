97661e1a0ecabb3b19a7a2321509555a247af6ad
Karmavir Joshi <karmavirj@live.com>
2024-05-17 12:41:08 +0530

    Added WebSocket server and package details
    A WebSocket server has been implemented in index.js, which listens for incoming messages and responds accordingly. Additionally, a new .gitignore file has been created to ignore node_modules/, pnpm-lock.yaml, and yarn.lock files. The package.json file now includes necessary dependencies and scripts for the project.

---
+++ index.js
@@ -0,0 +1,12 @@
+import WebSocket , {WebSocketServer} from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        console.log(`Received message => ${message}`);
+        ws.send(`Hello, you sent => ${message}`);
+    });
+    ws.send("Hello, I am a WebSocket server");
+    });
+
