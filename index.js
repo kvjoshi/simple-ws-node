f1a90121d69958d0e383e60811d614326773ff14
Karmavir Joshi <karmavirj@live.com>
2024-05-17 19:10:17 +0530

    Enhanced WebSocket server functionality
    The WebSocket server has been significantly improved. New features include client tracking, per message deflate, and handling of various events such as connection upgrades, closures, errors, and unexpected responses. Additionally, a new event for client errors has been introduced.

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import WebSocket , {WebSocketServer} from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 8080,
+    clientTracking: true,
+    perMessageDeflate:true,
+
+ });
 try{
 wss.on("connection", (ws) => {
+    wss.clients.forEach((client) => {
+        console.log(client);
+    });
+    ws.on("upgrade", (request, socket, head) => {
+        console.log("upgrade");
+    });
     ws.on("message", (message) => {
         console.log(`Received message => ${message}`);
         ws.send(`Hello, you sent => ${message}`);
     });
+    ws.on("close", (reason) => {
+        console.log("close" , reason);
+    });
+    ws.on("error", (err) => {
+        console.log(err);
+    });
+    ws.on("unexpected-response", (request, response) => {
+        console.log("unexpected-response", request, response);
+    });
+
     ws.send("Hello, I am a WebSocket server");
+    });
+wss.on("wsClientError", (err) => {
+        console.log("wsCLientError ",err);
     });
 }
 catch(err){
